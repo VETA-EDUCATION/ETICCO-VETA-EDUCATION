0ded8eacce1377d32c6f01bac12567f725fdd284
VETAEDUCATION <accounts@australiaveta.com.co>
2021-12-01 16:28:08 -0500

    Removing Upload Files

--- upload/upgrades/temp/zXrSRH/SugarModules/relationships/language/application.php
+++
@@ -1,5 +0,0 @@
-<?php
-//THIS FILE IS AUTO GENERATED, DO NOT MODIFY
-$app_list_strings['record_type_display']['Veta_Liquidacion'] = 'Liquidacion';
-$app_list_strings['parent_type_display']['Veta_Liquidacion'] = 'Liquidacion';
-$app_list_strings['record_type_display_notes']['Veta_Liquidacion'] = 'Liquidacion';
