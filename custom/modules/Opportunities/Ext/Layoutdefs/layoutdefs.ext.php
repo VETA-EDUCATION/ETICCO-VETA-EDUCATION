269d994e273844ee802ea37730efa244b558f9fd
VETAEDUCATION <94147464+VETAEDUCATION@users.noreply.github.com>
2022-01-12 22:23:30 -0500

    Update layoutdefs.ext.php

--- custom/modules/Opportunities/Ext/Layoutdefs/layoutdefs.ext.php
+++ custom/modules/Opportunities/Ext/Layoutdefs/layoutdefs.ext.php
@@ -159,7 +159,7 @@ $layout_defs["Opportunities"]["subpanel_setup"]['veta_serviciocliente_opportunit
 unset($layout_defs['Opportunities']['subpanel_setup']['contacts']);
 unset($layout_defs['Opportunities']['subpanel_setup']['leads']);
 unset($layout_defs['Opportunities']['subpanel_setup']['documents']);
-unset($layout_defs['Opportunities']['subpanel_setup']['veta_pagocolegios_opportunities']);
+//unset($layout_defs['Opportunities']['subpanel_setup']['veta_pagocolegios_opportunities']);
 
 
 
