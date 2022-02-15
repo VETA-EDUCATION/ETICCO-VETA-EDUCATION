41a71781b6c1bd3efbcac1911dba8a02a9204a0e
Diego Ortiz <dmortizr@gmail.com>
2022-02-15 17:26:05 -0300

    Creamos estado Nuevo Proceso de Ventas para visas y servicio al cliente, tambien implementamos estado Descartada por nuevo curso para las aplicaciones

--- custom/include/language/es_ES.lang.php
+++ custom/include/language/es_ES.lang.php
@@ -211,6 +211,7 @@ $GLOBALS[ 'app_list_strings' ][ 'estado_aplicacion_list_2' ] = array(
     'Aplicacion_Pendiente'  => 'Aplicacion Pendiente',
     'Aplicacion_Enviada'    => 'Aplicación Enviada',
     'Aplicacion_Descartada' => 'Aplicacion Descartada',
+    'Descartada_Nuevo_Curso' => 'Descartada por nuevo curso',
 );
 $GLOBALS[ 'app_list_strings' ][ 'estado_loo_list_2' ]        = array(
     'Requerimiento_LOO'                 => 'Requerimiento para Carta de Oferta',
@@ -247,6 +248,7 @@ $GLOBALS[ 'app_list_strings' ][ 'estado_visa_list' ] = array(
     'Visa_Negada'              => 'Visa Negada',
     'Visa_Reaplicable'         => 'Visa Reaplicable',
     'Descartada'               => 'Descartada',
+    'Nuevo_Proceso_Ventas' => 'Nuevo Proceso de Ventas',
 );
 
 $GLOBALS[ 'app_list_strings' ][ 'estado_pago_colegios_list' ] = array(
@@ -602,6 +604,7 @@ $GLOBALS['app_list_strings']['estado_servicio_al_cliente_list']=array (
   'Week1' => 'Week 1',
   'Week2' => 'Week 2',
   'Week3' => 'Week 3',
+  'Nuevo_Proceso_Ventas' => 'Nuevo Proceso de Ventas',
 );
 $GLOBALS['app_list_strings']['localizacion_list']=array (
     'Offshore' => 'Offshore',
